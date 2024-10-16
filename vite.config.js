313d52939d2d29c152e34771144734a20605e83a
mraihanhatim <raihan04042001@gmail.com>
2024-10-16 13:11:14 +0700

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/zaalart-photography/", // Change this to your repo name
 });
